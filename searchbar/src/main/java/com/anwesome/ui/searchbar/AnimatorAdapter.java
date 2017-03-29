256a0c634d03fd51fd316da6dd0b8efa2dc24590
Anwesh43 <anweshthecool0@gmail.com>
2017-03-29 16:40:41 +0530

    Adding AnimatorAdapter

---
+++ searchbar/src/main/java/com/anwesome/ui/searchbar/AnimatorAdapter.java
@@ -0,0 +1,25 @@
+package com.anwesome.ui.searchbar;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+
+/**
+ * Created by anweshmishra on 29/03/17.
+ */
+public class AnimatorAdapter implements Animator.AnimatorListener,ValueAnimator.AnimatorUpdateListener {
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+    }
+    public void onAnimationEnd(Animator animator) {
+
+    }
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+    public void onAnimationCancel(Animator animator) {
+
+    }
+    public void onAnimationStart(Animator animator) {
+
+    }
+}
