b1781668afb0cb15935545039070dc8f71993fcc
Anwesh43 <anweshthecool0@gmail.com>
2017-03-29 20:00:17 +0530

    Adding TextWatcher for TextView

--- searchbar/src/main/java/com/anwesome/ui/searchbar/SearchTextViewWatcher.java
+++ searchbar/src/main/java/com/anwesome/ui/searchbar/SearchTextViewWatcher.java
@@ -1,7 +1,34 @@
 package com.anwesome.ui.searchbar;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import java.util.List;
+
 /**
  * Created by anweshmishra on 29/03/17.
  */
-public class SearchTextViewWatcher {
+public class SearchTextViewWatcher implements TextWatcher{
+    private DataSource dataSource;
+    private OnMatchListener onMatchListener;
+    public SearchTextViewWatcher(DataSource dataSource,OnMatchListener onMatchListener) {
+        this.dataSource = dataSource;
+        this.onMatchListener = onMatchListener;
+    }
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        if(dataSource!=null && onMatchListener!=null) {
+            List<String> results = dataSource.matchForResults(charSequence.toString());
+            onMatchListener.onMatchResults(results);
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
 }
