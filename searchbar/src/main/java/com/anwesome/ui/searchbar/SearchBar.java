3b1f9b7d27a9c17fac71d3ba098bee950a7b441d
Anwesh43 <anweshthecool0@gmail.com>
2017-03-29 19:03:29 +0530

    adding searchtextview

--- searchbar/src/main/java/com/anwesome/ui/searchbar/SearchBar.java
+++ searchbar/src/main/java/com/anwesome/ui/searchbar/SearchBar.java
@@ -3,7 +3,9 @@ package com.anwesome.ui.searchbar;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
 /**
@@ -11,6 +13,7 @@ import android.widget.RelativeLayout;
  */
 public class SearchBar {
     private Activity activity;
+    private SearchTextView searchTextView;
     private RelativeLayout relativeLayout;
     private SearchIcon searchIcon;
     private int barColor = Color.parseColor("#3F51B5");
@@ -19,16 +22,27 @@ public class SearchBar {
     }
     public void show() {
         if(relativeLayout == null) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
             relativeLayout = new RelativeLayout(activity);
             Point dimension = DimensionsUtil.getDimensions(activity);
             int w = dimension.x,h = dimension.y;
             relativeLayout.setBackgroundColor(barColor);
             activity.addContentView(relativeLayout,new ViewGroup.LayoutParams(w,h/10));
             searchIcon = new SearchIcon(activity,4*w/5);
+            searchTextView = new SearchTextView(activity);
             relativeLayout.addView(searchIcon,new ViewGroup.LayoutParams(w/6,w/6));
+            relativeLayout.addView(searchTextView,new ViewGroup.LayoutParams(7*w/10,h/20));
             searchIcon.setRotation(-45);
             searchIcon.setX(2*w/5);
             searchIcon.setY(h/10-w/8);
+            searchTextView.setX(w/10);
+            searchTextView.setY(h/40);
+            searchIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    searchTextView.startShowing();
+                }
+            });
         }
     }
 }
