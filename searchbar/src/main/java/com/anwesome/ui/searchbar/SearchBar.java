b9625ad04f18a54c82e878a1c0ea32fc80cebdb4
Anwesh43 <anweshthecool0@gmail.com>
2017-03-29 16:27:21 +0530

    adding searchbar module

---
+++ searchbar/src/main/java/com/anwesome/ui/searchbar/SearchBar.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.searchbar;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 29/03/17.
+ */
+public class SearchBar {
+    private Activity activity;
+    private SearchBar(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
