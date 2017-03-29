8e7d3713bb425dc112b95410787976d2da720ac0
Anwesh43 <anweshthecool0@gmail.com>
2017-03-29 18:42:05 +0530

    initializing relative layout to fix the null pointer issue, rotating, repositioning and resizing the search icon

--- searchbar/src/main/java/com/anwesome/ui/searchbar/SearchBar.java
+++ searchbar/src/main/java/com/anwesome/ui/searchbar/SearchBar.java
@@ -14,19 +14,21 @@ public class SearchBar {
     private RelativeLayout relativeLayout;
     private SearchIcon searchIcon;
     private int barColor = Color.parseColor("#3F51B5");
-    private SearchBar(Activity activity) {
+    public SearchBar(Activity activity) {
         this.activity = activity;
     }
     public void show() {
         if(relativeLayout == null) {
+            relativeLayout = new RelativeLayout(activity);
             Point dimension = DimensionsUtil.getDimensions(activity);
             int w = dimension.x,h = dimension.y;
             relativeLayout.setBackgroundColor(barColor);
             activity.addContentView(relativeLayout,new ViewGroup.LayoutParams(w,h/10));
             searchIcon = new SearchIcon(activity,4*w/5);
-            relativeLayout.addView(searchIcon,new ViewGroup.LayoutParams(w/7,w/7));
-            searchIcon.setX(w/2);
-            searchIcon.setY(h/10-w/5);
+            relativeLayout.addView(searchIcon,new ViewGroup.LayoutParams(w/6,w/6));
+            searchIcon.setRotation(-45);
+            searchIcon.setX(2*w/5);
+            searchIcon.setY(h/10-w/8);
         }
     }
 }
